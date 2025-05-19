e63f43809e3eae8756fc49529567d14e8e7b64d2
Myktybek7 <myktybekabdykaiymov@gmail.com>
2025-05-19 14:16:02 +0600

    Initial commit with dependencies.

---
+++ src/test/java/alatoo/edu/medicalmanagementsystem/MedicalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package alatoo.edu.medicalmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
