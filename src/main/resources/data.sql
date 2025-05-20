e6cba670668b6707977f92564186c88b19f945fe
Myktybek7 <myktybekabdykaiymov@gmail.com>
2025-05-20 17:26:18 +0600

    Created home page controller and html page. Additional data for department.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO department (id, name, description) VALUES (1, 'Офтальмология', 'Отделение глазных болезней');
+INSERT INTO department (id, name, description) VALUES (2, 'Стоматология', 'Отделение стоматологических услуг');
+INSERT INTO department (id, name, description) VALUES (3, 'Дерматология', 'Отделение кожных болезней');
